638ddc79a843afed220d7a031ebad981702928ac
Martin Roy <9348202@cstjean.qc.ca>
2022-05-01 22:53:58 -0400

    salut

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello les amis de Mart !!!!! !');
+  res.send('salut comment ca va les amis de Mart ???');
 })
 
 app.listen(80)
