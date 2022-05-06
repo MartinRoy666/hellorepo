1980c32e8a031f99592358a13bfd6d2bf9d125cc
Martin Roy <9348202@cstjean.qc.ca>
2022-05-06 10:35:53 -0400

    mais ou est donc ...

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('salut comment ca va LE MONDE ?');
+  res.send('mais ou et donc car ni or');
 })
 
 app.listen(80)
